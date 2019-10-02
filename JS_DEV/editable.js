ddc7bfd4b23b96b64ce2ff5680ef93b3d8e39092
IvanMejico <ivan.mejico21@gmail.com>
2019-10-02 14:38:36 +0800

    editable.js refactor

--- JS_DEV/editable.js
+++ JS_DEV/editable.js
@@ -15,91 +15,132 @@
  * 9) When cancel is clicked, replace each respective elements with text without saving the inputted data.
  */
 
-// REMEMBER TO SET THE FORM NAME IN EVERY CONTROL
 
-
-var inlineEditRowContents = {};
+var tempRowContentBuffer = {};
 
 // form Constants
 const editOptions = {
-    'getForm' : function (rowId) {
+    getForm : function (rowId) {
         return `<form id=form${rowId}></form>`
     },
-    'getText' : function(rowId, i) {
-        name = document.getElementById(rowId).children[i].dataset.inlinename;
-        return `<input type="text" name="${name}" value="${inlineEditRowContents[rowId][i]}" form="form${rowId}"/>`;
+    getText : function(rowId, cellName) {
+        return `<input type="text" name="${cellName}" value="${tempRowContentBuffer[rowId][cellName]}" form="form${rowId}"/>`; // R
     },
-    'getSelect' : function(rowId, i) {
-        name = document.getElementById(rowId).children[i].dataset.inlinename;
+    getSelect : function(rowId, cellName) {
         var lec="", lab="";
-        inlineEditRowContents[rowId][i] == "Lecture" ? lec="selected " : lab="selected ";
-        return `<select name="${name}" form="form${rowId}"><option ${lec} value="lecture">Lecture</option><option ${lab}value="laboratory">Laboratory</option></select>`            
+        tempRowContentBuffer[rowId][cellName] == "Lecture" ? lec="selected " : lab="selected ";
+        return `<select name="${cellName}" form="form${rowId}"><option ${lec}value="lecture">Lecture</option><option ${lab}value="laboratory">Laboratory</option></select>`            
     },
-    'getNumber' : function(rowId, i) {
-        name = document.getElementById(rowId).children[i].dataset.inlinename;
-        return `<input type="number" name="${name}" form="form${rowId}" value="${inlineEditRowContents[rowId][i]}">`
+    getNumber : function(rowId, cellName) {
+        return `<input type="number" name="${cellName}" form="form${rowId}" value="${tempRowContentBuffer[rowId][cellName]}">` // R
     },
-    'getButtons' : function(rowId) {
+    getButtons : function(rowId) {
         return `<div class="table-action"><a class="table-save" form="form${rowId}"><i class="flaticon-save"></i></a><a class="table-cancel" form="form${rowId}"><i class="flaticon-edit-tools"></i></a></div>`;
     },
-    'getDefaultButtons' : function () {
+    getDefaultButtons : function () {
         return `<div class="table-action"><a class="table-edit"><i class="flaticon-pencil-edit-button"></i></a><a class="table-delete"><i class="flaticon-garbage"></i></a></div>`;
     }
 }
-const buttons = {
-    "edit": "flaticon-pencil-edit-button",
-    "delete": "flaticon-garbage",
-    "save": "flaticon-save",
-    "cancel": "flaticon-edit-tools"
+const icons = {
+    edit: "flaticon-pencil-edit-button",
+    delete: "flaticon-garbage",
+    save: "flaticon-save",
+    cancel: "flaticon-edit-tools"
 }
 // Form literals for generating a new row
 const formLiterals = {
     form: '<form id="formNewRecord"></form>',
     subjectCode: '<input type="text name="subjectcode" form="formNewRecord"/>',
     subjectTitle: '<input type="text" name="subject" form="formNewRecord"/>',
-    subjectType: '<select name="subjecttype" form="formCPEM2113"><option selected value="lecture">Lecture</option><option value="laboratory">Laboratory</option></select>',
-    subjectUnits: '<input type="number" name="units" form="formCPEM2113" value="1"/>',
+    subjectType: '<select name="subjecttype" form="formNewRecord"><option selected value="lecture">Lecture</option><option value="laboratory">Laboratory</option></select>',
+    subjectUnits: '<input type="number" name="units" form="formNewRecord" value="1"/>',
 }
 
 
 
-// Helper Functions
-function getTable(addButton) {
-    return addButton.parentNode.parentNode.parentNode.parentNode;
+//*** HELPER FUNCTIONS ***
+
+/**
+ * Returns the parent table for the passed "Add Row" button.
+ * @param {HTMLAnchorElement} addButton "Add Row" button for the table.
+ */
+function getParentTable(addButton) {
+    // Extract the table head element that contains the addButton
+    let thead = addButton.parentNode.parentNode.parentNode;
+    
+    // Extract the specific table that contains the table head
+    let table = thead.parentNode;
+
+    return table;
 }
 
-function getRow(button) {
-    var cell = button.parentNode.parentNode;
-    var row = cell.parentNode;
+/**
+ * Returns the specific table row where the passed button is located.
+ * @param {HTMLLinkElement} button Row control button.
+ */
+function getTableRow(button) {
+    // Extract the containing table cell
+    let cell = button.parentNode.parentNode;
+
+    // Extract the containing table row
+    let row = cell.parentNode;
+
     return row;
 }
 
-function deleteRow(row) {
+/**
+ * Deletes a row from the its parent table.
+ * @param {HTMLTableRowElement} row Table row to be deleted.
+ */
+function deleteTableRow(row) {
+	
+    // Delete the object from the temporary buffer
+    if (tempRowContentBuffer[row.id] !== undefined)
+        delete tempRowContentBuffer[row.id];
+
     row.parentNode.removeChild(row);
+
+    // *Probably write a code that performs AJAX requests here to physically delete the row data in the database.
 }
 
-function inlineUpdateCell(tableCell, i, rowId) {
+/**
+ * Updates cell with matching form elements designated in the custom data attributes.
+ * @param {HTMLTableCellElement} tableCellObj HTML <td> DOM object
+ * @param {HTMLTableRowElement} tableRowObj HTML <tr> DOM object
+ */
+function inlineUpdateCell(tableCellObj, tableRowObj) {
+    rowId = tableRowObj.id;
+    cellName = tableCellObj.dataset.inlinename;
+
     var cellContent = "";
-    // Set the form
-    if(i === 0) {
-        cellContent += editOptions.getForm(rowId);
+    
+    // Determine if the cell is the first child of the row
+    if(tableCellObj.cellIndex === 0) {
+       cellContent += editOptions.getForm(rowId);
     }
-    switch(tableCell.dataset.inlinetype) {
+    
+    switch(tableCellObj.dataset.inlinetype) {
         case "text":
-            cellContent += editOptions.getText(rowId, i);
+            cellContent += editOptions.getText(rowId, cellName);
             break;
         case "select":
-            cellContent += editOptions.getSelect(rowId, i);
+            cellContent += editOptions.getSelect(rowId, cellName);
             break;
         case "number":
-            cellContent += editOptions.getNumber(rowId, i);
+            cellContent += editOptions.getNumber(rowId, cellName);
             break;
         case "buttons":
             cellContent += editOptions.getButtons(rowId);
             break;
     }
-    tableCell.innerHTML = cellContent;
+    tableCellObj.innerHTML = cellContent;
 }
+
+/**
+ * Generates a split button for table control.
+ * @param {string} button1 button name ['edit', 'delete', 'save', 'cancel']
+ * @param {string} button2 button name ['edit', 'delete', 'save', 'cancel']
+ */
 function generateButtons(button1, button2) {
     var container = document.createElement('div');
     container.classList.add('table-action');
@@ -107,12 +148,12 @@ function generateButtons(button1, button2) {
     var b1 = document.createElement('a');
     b1.classList.add(`table-${button1}`);
     var b1Logo = document.createElement('i');
-    b1Logo.classList.add(buttons[button1]);
+    b1Logo.classList.add(icons[button1]);
 
     var b2 = document.createElement('a');
     b2.classList.add(`table-${button2}`);
     var b2Logo = document.createElement('i');
-    b2Logo.classList.add(buttons[button2]);
+    b2Logo.classList.add(icons[button2]);
     
     b1.appendChild(b1Logo);
     b2.appendChild(b2Logo);
@@ -129,6 +170,12 @@ function generateButtons(button1, button2) {
     
     return container;
 }
+
+/**
+ * Generates a table data cell with custom dataset values.
+ * @param {string} inlineType 
+ * @param {string} inlineName 
+ */
 function generateCell(inlineType, inlineName) {
     var cell = document.createElement('td');
     cell.dataset.inlinetype = inlineType;
@@ -137,44 +184,29 @@ function generateCell(inlineType, inlineName) {
 }
 
 
+//*** Main ***
 
-// Main 
-window.onload = function() {
-    var editButton = document.getElementsByClassName('table-edit');
-    var deleteButton = document.getElementsByClassName('table-delete');
-    var addButton = document.getElementsByClassName('table-add');
-
-    // Add event listener to every edit button on the table
-    Array.prototype.forEach.call(editButton, function(b) {
-        b.addEventListener('click', inlineEdit);
-    });
-
-    // Add event istener to every delete button on the table
-    Array.prototype.forEach.call(deleteButton, function(b) {
-        b.addEventListener('click', inlineDelete);
-    });
-
-    Array.prototype.forEach.call(addButton, function(b) {
-        b.addEventListener('click', addRow);
-    });
-}
-
+/**
+ * Handles table button inline editing. Assigned to a button event handler.
+ */
 function inlineEdit() {
-    var tableRow = getRow(this);
-    var rowId = tableRow.id;
+    var tableRowObj = getTableRow(this);
+    var rowId = tableRowObj.id;
   
-    inlineEditRowContents[rowId] = {};
+    // Initialize row temporary row data
+    tempRowContentBuffer[rowId] = {};
 
-    for (var i=0; i < tableRow.children.length; i++) {
-        var tableCell = tableRow.children[i];
+    for (var i=0; i < tableRowObj.children.length; i++) {
+        var tableCellObj = tableRowObj.children[i];
+        var cellName = tableCellObj.dataset.inlinename;
+        
+        // Assign the value of the table in a global buffer array for simultaneous editing
+        if(tableCellObj.innerText != "")
+            tempRowContentBuffer[rowId][cellName] = tableCellObj.innerText;
 
-        // Assign the value of the table in a global array for simultaenous editing
-        if(tableCell.innerText != "")
-            inlineEditRowContents[rowId][i] = tableCell.innerText;
-
-        inlineUpdateCell(tableCell, i, rowId);
+        inlineUpdateCell(tableCellObj, tableRowObj);
     }
-
+    
     // Add event handler for save and cancel button
     var save = document.querySelector(`#${rowId} .table-action .table-save`);
     var cancel = document.querySelector(`#${rowId} .table-action .table-cancel`);
@@ -182,30 +214,39 @@ function inlineEdit() {
     cancel.addEventListener('click', inlineCancel);
 }
 
-
-
+/**
+ * Handles row delete.
+ */
 function inlineDelete() {
-    var tableRow = getRow(this);
-    deleteRow(tableRow);
+    var tableRow = getTableRow(this);
+    deleteTableRow(tableRow);
 }
 
+/**
+ * Handles AJAX requests for saving files to the database.
+ */
 function inlineSave() {
     console.log('save');
 }
 
+/**
+ * Cancels row edit.
+ */
 function inlineCancel() {
-    var tableRow = getRow(this);
+    var tableRow = getTableRow(this);
     var rowId = tableRow.id;
 
-    if(inlineEditRowContents[rowId] === undefined) {
+    if(tempRowContentBuffer[rowId] === undefined) {
         tableRow.parentNode.removeChild(tableRow);
     } else {
         for(var i=0; i < tableRow.children.length; i++) {
-            var tableCell = tableRow.children[i];
-            if(tableCell.dataset.inlinetype != "buttons") {
-                tableCell.innerHTML = inlineEditRowContents[rowId][i];
+            var tableCellObj = tableRow.children[i];
+            var cellName = tableCellObj.dataset.inlinename;
+            
+            if(tableCellObj.dataset.inlinetype != "buttons") {
+                tableCellObj.innerHTML = tempRowContentBuffer[rowId][cellName];
             } else {
-                tableCell.innerHTML = editOptions.getDefaultButtons();
+                tableCellObj.innerHTML = editOptions.getDefaultButtons();
             }
         }
         // Add event handler for save and cancel button
@@ -216,7 +257,9 @@ function inlineCancel() {
     }
 }
 
-
+/**
+ * Adds row with empty form elements.
+ */
 function addRow() {
     var row = document.createElement('tr');
     row.id = "newRow";
@@ -234,10 +277,8 @@ function addRow() {
     var units = generateCell('number', 'units');
     units.innerHTML = formLiterals.subjectUnits;
 
-    // SEE HOW TO SETUP TABLE BUTTONS FOR NEW ROW - save and delete
     var buttons = generateCell('buttons', 'actionbuttons');
     buttons.appendChild(generateButtons('save','cancel'));
-
 
     // Append cells to row
     row.appendChild(code);
@@ -247,6 +288,28 @@ function addRow() {
     row.appendChild(buttons);
 
     // Append row to the table
-    table = getTable(this);
+    table = getParentTable(this);
     table.children[1].appendChild(row);
+}
+
+
+window.onload = function() {
+    var editButton = document.getElementsByClassName('table-edit');
+    var deleteButton = document.getElementsByClassName('table-delete');
+    var addButton = document.getElementsByClassName('table-add');
+
+    // Add event listener to every edit button on the table
+    Array.prototype.forEach.call(editButton, function(b) {
+        b.addEventListener('click', inlineEdit);
+    });
+
+    // Add event istener to every delete button on the table
+    Array.prototype.forEach.call(deleteButton, function(b) {
+        b.addEventListener('click', inlineDelete);
+    });
+
+    // Add event listener to every add button on the table
+    Array.prototype.forEach.call(addButton, function(b) {
+        b.addEventListener('click', addRow);
+    });
 }
